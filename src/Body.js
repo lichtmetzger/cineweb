7adacb29e5e724cc152fd494a6080d0afafca57c
Danny Schmarsel <dsc@qbus.de>
2023-12-15 14:10:43 +0100

    Add full movie cards and styling

--- src/Body.js
+++ src/Body.js
@@ -18,6 +18,22 @@ class Body {
     const api = new Api;
     
     api.getMovies().then((movies) => {
+
+      // Sort movies by title
+      movies.sort((a, b) => {
+        const nameA = a.title.toUpperCase(); // ignore case
+        const nameB = b.title.toUpperCase(); // ignore case
+    
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+        
+        return 0; // names must be equal
+      });
+
       movies.forEach(movieData => {
          const movie = new Movie(movieData);
          this.output.innerHTML += movie.getCard();
--- src/Movie.js
+++ src/Movie.js
@@ -6,9 +6,58 @@ class Movie {
 	}
 
   getCard() {
-    console.log(this.movieData);
-    const cardHtml = `<div class="card">${this.getTitle()}</div>`;
+    //console.log(this.movieData);
+
+    const cardHtml = 
+    `<div class="card" data-id="${this.getId()}" data-event-id="${this.getEventId()}">
+      <div class="thumb"><img src="${this.getThumbUrl()}" alt="${this.getTitle()}" /></div>
+      <h3 class="title">${this.getTitle()}</h3>
+      <h4 class="subtitle">${this.getSubtitle()}</h4>
+      <span class="date">Presale starts: ${this.getStartDate()}</span>
+    </div>`;
+
+
     return cardHtml;
+  }
+
+  getEventId() {
+    return this.movieData.event;
+  }
+
+  getId() {
+    return this.movieData.id;
+  }
+
+  getStartDate() {
+    const dateRaw = new Date(this.movieData.startDate);
+    let dateParsed = "No date available (yet)";
+
+    if(dateRaw) {
+      const options = {
+        timeZone: 'Europe/Berlin',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+      };
+
+      try {
+        dateParsed = new Intl.DateTimeFormat('de-DE', options).format(dateRaw);
+      } catch (error) {
+        dateParsed = "No date available (yet)";
+      } 
+    }
+
+    return dateParsed;
+  }
+
+  getSubtitle() {
+    return this.movieData.subtitle;
+  }
+
+  getThumbUrl() {
+    return this.movieData.poster;
   }
 
   getTitle() {
