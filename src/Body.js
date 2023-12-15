b788aeb08a1998a3cb30cb99a37088fb4ecc0cdc
Danny Schmarsel <dsc@qbus.de>
2023-12-15 19:24:22 +0100

    Add first-run message

--- src/Body.js
+++ src/Body.js
@@ -1,15 +1,33 @@
 import Api from "./Api";
 import Movie from "./Movie";
 import ShowTimes from "./ShowTimes";
+import LocalStorage from "./LocalStorage";
 
 class Body {
   constructor() {
 		this.output = document.getElementById('body');
+    this.storage = new LocalStorage();
+
+    this.handleFirstRun();
 
     document.addEventListener('cinemaChosen', (event) => {
       this.addMovieCards();
     });
 	}
+
+  handleFirstRun() {
+    // Check if we have a cinema set, show a message if not.
+    const cinemaId = this.storage.getValue('cinemaId');
+
+    if(!cinemaId) {
+      let note = document.createElement('div');
+      note.setAttribute('class', 'note');
+      note.innerHTML = 'Welcome to CINEWEB, an alternative frontend for booking Cinestar tickets.<br><br>'
+      note.innerHTML += '<strong>Please pick a cinema at the top to see all current movies and events.</strong>';
+
+      this.output.append(note);
+    }
+  }
 
   addMovieCards() {
     // Clear results
