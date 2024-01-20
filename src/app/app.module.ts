5f7f17b7c69500206dd3da4e7c0d860dd2e0bb23
Danny Schmarsel <dsc@qbus.de>
2024-01-20 19:23:34 +0100

    Add cinema-selector
    It's static for now until I've learned how to make requests to the API in Angular.
    
    Also, the button doesn't have a function yet, because I need to learn template references later.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
+import { CinemaSelectorComponent } from './cinema-selector/cinema-selector.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
         MainComponent,
-        FooterComponent
+        FooterComponent,
+        CinemaSelectorComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        FormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
